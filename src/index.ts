9da315bd69485750d5317419efc9096ad1a99c4b
Antony Bartlett <antony.bartlett@innovyze.com>
2020-05-19 17:35:56 +0100

    Write out the angle C

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const sketch = (p: p5): void => {
     p.line(pt1.x, pt1.y, pt2.x, pt2.y);
   };
 
-  const labelAngle = (label: string, points: p5.Vector[], index: number): void => {
+  const labelAngle = (label: string, points: p5.Vector[], index: number): number => {
 
     const point = points[index];
 
@@ -19,20 +19,22 @@ const sketch = (p: p5): void => {
 
     const textAdjustment = (new p5.Vector()).set(-p.textWidth(label), p.textSize()).mult(0.5);
 
-    const labelPosition = sum.setMag(20).add(point).add(textAdjustment);
+    const labelPosition = sum.copy().setMag(20).add(point).add(textAdjustment);
 
     p.text(label, labelPosition.x, labelPosition.y);
+    return sum.heading();
   };
 
   const points: p5.Vector[] = [
-    (new p5.Vector()).set(300, 150),
     (new p5.Vector()).set(500, 100),
-    (new p5.Vector()).set(450, 300)
+    (new p5.Vector()).set(450, 300),
+    (new p5.Vector()).set(300, 150),
   ];
 
   const doDraw = (): void => {
     p.background(200);
 
+    // Draw points
     p.noStroke();
     p.fill(255, 0, 0, 128);
     
@@ -40,15 +42,24 @@ const sketch = (p: p5): void => {
       p.rect(point.x, point.y, rectRadius, rectRadius);
     });
 
+    // Draw lines
     p.stroke(0);
     line(points[0], points[1]);
     line(points[1], points[2]);
     line(points[0], points[2]);
 
+    // Label points
     p.fill(0);
     labelAngle('A', points, 0);
     labelAngle('B', points, 1);
-    labelAngle('C', points, 2);
+
+    // Write out what we know
+    const C = labelAngle('C', points, 2);
+    const Cabs = Math.abs(C);
+    const Cdeg = 180 * Cabs / Math.PI;
+
+    const aboutC = `C = ${Cabs.toFixed(2)} radians = ${Cdeg.toFixed(0)} degrees`;
+    p.text(aboutC, 10, 30);
   };
 
   p.setup = (): void => {
